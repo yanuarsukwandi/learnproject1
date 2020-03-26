83f6bde693fccbb4825f9db637cac2588f3ca952
Yanuar Gandhira <yanuar.sukwandi@gmail.com>
2020-03-26 11:49:57 +0700

    secondcommit_11:49

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Navbar from "./components/layout/Navbar.js";
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
+        <Navbar />
       </div>
     );
   }
