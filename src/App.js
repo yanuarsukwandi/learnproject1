59023a79216dedc8c4d43b0e47f6cc7cff7f5ab7
Yanuar Gandhira <yanuar.sukwandi@gmail.com>
2020-03-26 14:32:51 +0700

    commit_14:32

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import Navbar from "./components/layout/Navbar.js";
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github-Finder" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   }
