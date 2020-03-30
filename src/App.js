9937d40e0b22c76ab4e06dd02a7a0d2b26ef6d10
Yanuar Gandhira <yanuar.sukwandi@gmail.com>
2020-03-30 11:15:57 +0700

    monday_11:15_30/03/2020

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
   // https get request by axios
   // dont forget to use componentDidMount if want to get request
@@ -45,6 +46,17 @@ class App extends Component {
     );
     this.setState({ user: res.data, loading: false });
   };
+
+  // get user repos
+  getUserRepos = async username => {
+    this.setState({ loading: "true" });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear User
   clearUsers = () => this.setState({ users: [], loading: false });
   // Alert
@@ -87,7 +99,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={this.state.user}
+                    repos={this.state.repos}
                     loading={this.state.loading}
                   />
                 )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,23 @@
 import React, { Fragment, Component } from "react";
 import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
+
+  static propTypes = {
+    repos: PropTypes.array.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -21,11 +31,12 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) {
       return <Spinner />;
     }
+
     return (
       <Fragment>
         <Link to="/" className="btn btn-light">
@@ -59,8 +70,40 @@ export class User extends Component {
             <a href={html_url} className="btn btn-dark my-1">
               <i className="fab fa-github"></i> Vist Github Pages
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username : </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company : </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website : </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers : {followers}</div>
+          <div className="badge badge-success">Following : {following}</div>
+          <div className="badge badge-dark">Public Repos : {public_repos}</div>
+          <div className="badge badge-light">Public Gists : {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
